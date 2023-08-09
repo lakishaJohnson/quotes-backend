1b4c05d5c0e843d341364ffecd731f52c837ab55
Lakisha J <lakishajohnson@pursuit.org>
2023-08-08 21:38:39 -0400

    Still working on app.js routes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //DEPENDENCIES
 const cors = require("cors");
 const express = require("express");
-const { getRandomQuote } = require("./queries/quotes");
 
 //CONFIGURE
 const app = express();
@@ -11,18 +10,21 @@ app.use(cors());
 app.use(express.json());
 
 //ROUTES
-app.get("/", async (req, res) => {
-  try {
-    const randomQuote = await getRandomQuote();
-    if (randomQuote) {
-      res.status(200).json(randomQuote);
-    } else {
-      res.status(404).json({ error: "No quotes found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Server error" });
-  }
-});
+app.get("/", (req,res) => {
+    res.send("Welcome")
+})
+// app.get("/", async (req, res) => {
+//     try {
+//       const randomQuote = await getRandomQuote();
+//       if (randomQuote) {
+//         res.status(200).json(randomQuote);
+//       } else {
+//         res.status(404).json({ error: "No quotes found" });
+//       }
+//     } catch (error) {
+//       res.status(500).json({ error: "Server error" });
+//     }
+//   });
 
 //QUOTES ROUTE
 const quotesController = require("./controllers/quotesController.js");
@@ -30,8 +32,8 @@ app.use("/quotes", quotesController);
 
 //404 PAGE
 app.get("*", (req, res) => {
-  res.status(404).send("ooops, Page not found");
-});
+    res.status(404).send("ooops, Page not found")
+})
 
 //EXPORT
 module.exports = app;
