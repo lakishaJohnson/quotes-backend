a5466b9acabdbaff5b90e1c621defb46d3da7b5d
Lakisha J <lakishajohnson@pursuit.org>
2023-08-08 20:42:59 -0400

    Updated root route to show random quote

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //DEPENDENCIES
 const cors = require("cors");
 const express = require("express");
+const { getRandomQuote } = require("./queries/quotes");
 
 //CONFIGURE
 const app = express();
@@ -10,9 +11,18 @@ app.use(cors());
 app.use(express.json());
 
 //ROUTES
-app.get("/", (req,res) => {
-    res.send("Welcome")
-})
+app.get("/", async (req, res) => {
+  try {
+    const randomQuote = await getRandomQuote();
+    if (randomQuote) {
+      res.status(200).json(randomQuote);
+    } else {
+      res.status(404).json({ error: "No quotes found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 //SONGS ROUTE
 // const songsController = require("./controllers/songController.jsx");
@@ -20,8 +30,8 @@ app.get("/", (req,res) => {
 
 //404 PAGE
 app.get("*", (req, res) => {
-    res.status(404).send("ooops, Page not found")
-})
+  res.status(404).send("ooops, Page not found");
+});
 
 //EXPORT
 module.exports = app;
